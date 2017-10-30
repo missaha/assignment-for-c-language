c69a26155492d0e03d3e80cf56e534a0814a71da
missaha <31724093+missaha@users.noreply.github.com>
2017-10-30 21:23:14 +0800

    Add files via upload

---
+++ 3_8.cpp
@@ -0,0 +1,12 @@
+#include <iostream.h>
+#include <math.h>
+void main()
+{
+int a,b;
+cout<<"please input (int)a,b";
+cin>>a>>b;
+if(a%b==0)
+	cout<<"a/b="<<a/b<<endl;
+else
+	cout<<"a/b="<<(int)a/b<<"бн"<<a%b<<endl;
+}
