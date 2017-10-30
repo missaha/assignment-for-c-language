8541a74df329e4cb8b1615b316b5ec17696e34e9
missaha <31724093+missaha@users.noreply.github.com>
2017-10-30 22:39:00 +0800

    Add files via upload

---
+++ 4_5.cpp
@@ -0,0 +1,27 @@
+#include<iostream.h>
+void main()
+{
+int x,y;
+cout<<"½á¹ûÊÇ:";
+for(x=0;x<=9;x=x++)
+{
+for(y=0;y<=9;y=y++)
+{
+if((10x+y)£¨10x+y£©<10&&(10x+y)£¨10x+y£©=10x+y)
+{
+cout<<10x+y<<"\t"
+}
+lese
+if((10x+y)£¨10x+y£©<100&&(10x+y)£¨10x+y£©>=10&&(10x+y)£¨10x+y£©%10=10x+y)
+{
+cout<<10x+y<<"\t"
+}
+lese
+if((10x+y)£¨10x+y)>=100&&(10x+y)£¨10x+y£©%100=10x+y)
+{
+cout<<10*x+y<<"\t"
+}
+}
+}
+cout<<"\n"
+}
