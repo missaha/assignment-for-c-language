e9bf53a8942e1ba70f0e278d803c5bf60e2b72ac
missaha <31724093+missaha@users.noreply.github.com>
2017-10-30 21:19:48 +0800

    Add files via upload

---
+++ 3_6.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+void main()
+{
+float r,s,c;
+int k;
+cin>>r>>k;
+switch(k=)
+{
+case"k=1":cout<<s=¦Ð*r*r<<endl;break;
+case"k=2":cout<<c=2*¦Ð*r<<endl;break;
+case"k=3":cout<<s=¦Ð*r*r<<c=2*¦Ð*r<<endl;break;
+default:cout<<"ÊäÈëÓÐ´íÎó£¡\n";
+}
+}
