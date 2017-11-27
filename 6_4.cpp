2c24b8465a602635d1effe717927b3d817529723
missaha <31724093+missaha@users.noreply.github.com>
2017-11-28 00:00:09 +0800

    Add files via upload

---
+++ 6_4.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream.h>
+
+#define N 20
+
+void main()
+
+{
+	int i;
+	int a[N];
+    a[0]=1;
+	a[1]=1;
+	for(i=2;i<N;i++)
+	   a[i]=a[i-1]+a[i-2];
+    for(i=0;i<N;i++)
+       cout<<a[i]<<" ";
+}
